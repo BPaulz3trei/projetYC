942f27800c7be9604bbc57dfc27ae13221157808
Paul Beyssac <236885389+BPaulz3trei@users.noreply.github.com>
2025-11-03 17:00:52 +0100

    devinette

--- devinette.c
+++ devinette.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+int num_secret=10;
+int trouve = 0;
+int num_u;
+while (trouve == 0){
+  scanf("Entrez Numéro : %d",&num_u);
+  if (num_u==num_secret){
+    trouve=1;
+    printf("Célébration");
+  }
+}
 int main() {
     printf("Hello World\n");
     return 0;
