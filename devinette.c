78badd3019dea0c5c2c3b329c1bb782e7f10e919
Paul Beyssac <236885389+BPaulz3trei@users.noreply.github.com>
2025-11-03 17:17:21 +0100

    Indications descriptives Paul

--- devinette.c
+++ devinette.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-int num_secret=10;
-int trouve = 0;
-int num_u;
-while (trouve == 0){
-  scanf("Entrez Numéro : %d",&num_u);
-  if (num_u==num_secret){
-    trouve=1;
-    printf("Célébration");
-  }
-}
+
 int main() {
-    printf("Hello World\n");
+  int num_secret=10;
+  int num_u;
+
+  if (num_u < num_secret){
+    printf("Le nombre à deviner est plus grand");
+  }else if (num_u > num_secret){
+    printf("Le nombre à deviner est plus petit");
+  }
     return 0;
 }
